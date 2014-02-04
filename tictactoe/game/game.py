a722167860dbb5a335d4b11498de0463be504251
Adam Duren <adamduren@gmail.com>
2014-02-04 00:07:53 -0600

    Implemented first version of AiPlayer

--- tictactoe/game/game.py
+++ tictactoe/game/game.py
@@ -12,17 +12,19 @@ class ConsoleGame(object):
             if board.is_terminal_state(state):
                 break
 
-            player = state['current_player']
+            player = board.to_move(state)
 
             print('{}\'s turn'.format(player.name))
 
             try:
-                state = board.result(state, player.make_turn_decision(state))
+                state = board.result(state, player.make_turn_decision(state, board))
             except ValueError as e:
                 print('{} Would you like to continue playing? '.format(e.message))
 
                 if raw_input() not in ['y', 'yes']:
                     break
 
-        if board.is_terminal_state(state):
+        if board.determine_winner(state):
             print('{} Wins!'.format(player.name))
+        else:
+            print('It\'s a draw')
--- tictactoe/game/player.py
+++ tictactoe/game/player.py
@@ -1,5 +1,7 @@
 from .utils import erase_last_line
 
+infinity = float('inf')
+
 
 class Player(object):
     name = None
@@ -9,7 +11,7 @@ class Player(object):
         self.name = name
         self.mark = mark
 
-    def make_turn_decision(self, state):
+    def make_turn_decision(self, state, board):
         decision = int(raw_input())
 
         # Clean up after ourselves
@@ -18,6 +20,45 @@ class Player(object):
         return decision
 
 
-# Todo: implement AI logic
 class AiPlayer(Player):
-    pass
+
+    def make_turn_decision(self, state, board):
+        player = board.to_move(state)
+
+        def max_value(state):
+            if board.is_terminal_state(state):
+                return board.utility(state, player.mark)
+
+            value = -infinity
+
+            for a in board.get_actions(state):
+                value = max(value, min_value(board.result(state, a)))
+
+            return value
+
+        def min_value(state):
+            if board.is_terminal_state(state):
+                return board.utility(state, player.mark)
+
+            value = infinity
+
+            for a in board.get_actions(state):
+                value = min(value, max_value(board.result(state, a)))
+
+            return value
+
+        max_v = -infinity
+        max_a = -1
+
+        actions = board.get_actions(state)
+
+        for a in actions:
+            cur_value = min_value(board.result(state, a))
+
+            if cur_value > max_v:
+                max_v = cur_value
+                max_a = a
+
+        decision = max_a
+
+        return decision
